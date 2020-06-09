08de4964039aba9f71e020f6b7cc032d513a9295
TheBeastLT <pauliox@beyond.lt>
2020-06-09 20:12:21 +0200

    [addon] updates static videos links

--- addon/moch/static.js
+++ addon/moch/static.js
@@ -1,7 +1,8 @@
+// https://gofile.io/d/ZYlcAI
 module.exports = {
-  DOWNLOADING: 'https://pictshare.net/i4q1aa.mp4/raw',
-  FAILED_DOWNLOAD: 'https://pictshare.net/efvr4p.mp4/raw',
-  FAILED_ACCESS: 'https://pictshare.net/unnwy8.mp4/raw',
-  FAILED_RAR: 'https://pictshare.net/2ldkfd.mp4/raw',
-  FAILED_UNEXPECTED: 'https://pictshare.net/r4ul1j.mp4/raw'
+  DOWNLOADING: 'https://srv-file9.gofile.io/download/ZYlcAI/downloaded.mp4',
+  FAILED_DOWNLOAD: 'https://srv-file9.gofile.io/download/ZYlcAI/download_failed.mp4',
+  FAILED_ACCESS: 'https://srv-file9.gofile.io/download/ZYlcAI/failed_access.mp4',
+  FAILED_RAR: 'https://srv-file9.gofile.io/download/ZYlcAI/failed_rar.mp4',
+  FAILED_UNEXPECTED: 'https://srv-file9.gofile.io/download/ZYlcAI/failed_unexpected.mp4'
 }
