76da5c9a198a81915e3cc37e7e8ba25011581f8e
TheBeastLT <pauliox@beyond.lt>
2020-06-23 14:44:40 +0200

    [addon] fixes static video mix up

--- addon/moch/static.js
+++ addon/moch/static.js
@@ -1,8 +1,8 @@
 const RESOLVER_HOST = process.env.RESOLVER_HOST || 'http://localhost:7050';
 
 module.exports = {
-  DOWNLOADING: `${RESOLVER_HOST}/videos/download_failed_v1.mp4`,
-  FAILED_DOWNLOAD: `${RESOLVER_HOST}/videos/downloaded_v1.mp4`,
+  DOWNLOADING: `${RESOLVER_HOST}/videos/downloading_v1.mp4`,
+  FAILED_DOWNLOAD: `${RESOLVER_HOST}/videos/download_failed_v1.mp4`,
   FAILED_ACCESS: `${RESOLVER_HOST}/videos/failed_access_v1.mp4`,
   FAILED_RAR: `${RESOLVER_HOST}/videos/failed_rar_v1.mp4`,
   FAILED_UNEXPECTED: `${RESOLVER_HOST}/videos/failed_unexpected_v1.mp4`
