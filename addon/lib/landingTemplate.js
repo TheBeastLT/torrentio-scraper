7ff2a3e208737b358bd4726c0fbc76d2cececb94
TheBeastLT <pauliox@beyond.lt>
2020-09-09 15:37:36 +0200

    [addon] fixes landing templates imports

--- addon/lib/landingTemplate.js
+++ addon/lib/landingTemplate.js
@@ -161,12 +161,6 @@ button:active {
   text-align: left;
 }
 
-.caret {
-  position: absolute;
-  left: 97%;
-  top: 45%;
-}
-
 .multiselect-container {
   border: 0;
   border-radius: 0;
@@ -231,11 +225,12 @@ function landingTemplate(manifest, config = {}) {
       <title>${manifest.name} - Stremio Addon</title>
       <link rel="shortcut icon" href="${logo}" type="image/x-icon">
       <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700&display=swap" rel="stylesheet">
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-      <script src="https://davidstutz.de/bootstrap-multiselect/docs/js/bootstrap-3.3.2.min.js"></script>
-      <link href="https://davidstutz.de/bootstrap-multiselect/docs/css/bootstrap-3.3.2.min.css" rel="stylesheet"/>
-      <script src="https://davidstutz.de/bootstrap-multiselect/dist/js/bootstrap-multiselect.js"></script>
-      <link href="https://davidstutz.de/bootstrap-multiselect/dist/css/bootstrap-multiselect.css" rel="stylesheet"/>
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+      <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet" >
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/0.9.15/js/bootstrap-multiselect.min.js"></script>
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/0.9.15/css/bootstrap-multiselect.css" rel="stylesheet"/>
       <style>${STYLESHEET}</style>
    </head>
 
