bee29eb5cf4194be63dfd30407fc72c04e3c9318
TheBeastLT <pauliox@beyond.lt>
2020-12-13 20:06:35 +0100

    [addon] include hash in too large request error

--- addon/moch/realdebrid.js
+++ addon/moch/realdebrid.js
@@ -49,7 +49,7 @@ async function _getInstantAvailable(hashes, apiKey, retries = 3) {
         if (retries > 0 && ['ESOCKETTIMEDOUT', 'EAI_AGAIN'].some(v => error.message && error.message.includes(v))) {
           return _getInstantAvailable(hashes, apiKey, retries - 1);
         }
-        console.warn('Failed RealDebrid cached torrent availability request: ', error.message);
+        console.warn(`Failed RealDebrid cached [${hashes[0]}] torrent availability request: `, error.message);
         return undefined;
       });
 }
