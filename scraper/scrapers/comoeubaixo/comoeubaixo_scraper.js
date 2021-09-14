50882c30a9c76f594a26c8f63beb3d4a41c2924f
TheBeastLT <pauliox@beyond.lt>
2021-09-14 15:13:39 +0200

    format comoeubaixo provider

--- scraper/scrapers/comoeubaixo/comoeubaixo_scraper.js
+++ scraper/scrapers/comoeubaixo/comoeubaixo_scraper.js
@@ -1,15 +1,14 @@
 const moment = require("moment");
 const Bottleneck = require("bottleneck");
-const leetx = require("./comoeubaixo_api");
+const comoeubaixo = require("./comoeubaixo_api");
 const { Type } = require("../../lib/types");
 const repository = require("../../lib/repository");
 const Promises = require("../../lib/promises");
 const { createTorrentEntry, checkAndUpdateTorrent } = require("../../lib/torrentEntries");
 const { updateCurrentSeeders, updateTorrentSize } = require("../../lib/torrent");
-const { getImdbId } = require("../../lib/metadata");
 
 const NAME = "ComoEuBaixo";
-const UNTIL_PAGE = 5;
+const UNTIL_PAGE = 3;
 const TYPE_MAPPING = typeMapping();
 
 const limiter = new Bottleneck({ maxConcurrent: 5 });
@@ -20,116 +19,95 @@ async function scrape() {
   console.log(`[${scrapeStart}] starting ${NAME} scrape...`);
 
   return scrapeLatestTorrents()
-    .then(() => {
-      lastScrape.lastScraped = scrapeStart;
-      return lastScrape.save();
-    })
-    .then(() => console.log(`[${moment()}] finished ${NAME} scrape`));
+      .then(() => {
+        lastScrape.lastScraped = scrapeStart;
+        return lastScrape.save();
+      })
+      .then(() => console.log(`[${moment()}] finished ${NAME} scrape`));
 }
 
 async function updateSeeders(torrent) {
-  return limiter.schedule(() => leetx.torrent(torrent.torrentId));
+  return limiter.schedule(() => comoeubaixo.torrent(torrent.torrentId));
 }
 
 async function scrapeLatestTorrents() {
   const allowedCategories = [
-    leetx.Categories.MOVIE,
-    leetx.Categories.TV,
-    leetx.Categories.DESENHOS
+    comoeubaixo.Categories.MOVIE,
+    comoeubaixo.Categories.TV,
+    comoeubaixo.Categories.DESENHOS
   ];
 
-  return Promises.sequence(
-    allowedCategories.map(
-      (category) => () => scrapeLatestTorrentsForCategory(category)
-    )
-  ).then((entries) => entries.reduce((a, b) => a.concat(b), []));
+  return Promises.sequence(allowedCategories
+          .map((category) => () => scrapeLatestTorrentsForCategory(category)))
+      .then((entries) => entries.reduce((a, b) => a.concat(b), []));
 }
 
-async function scrapeLatestTorrentsForCategory(category, page = 1) {
-  console.log({Scraper: `Scrapping ${NAME} ${category} category page ${page}`});
-  return leetx
-    .browse({ category, page })
-    .catch((error) => {
-      console.warn(
-        `Failed ${NAME} scrapping for [${page}] ${category} due: `,
-        error
-      );
-      return Promise.resolve([]);
-    })
-    .then((torrents) => Promise.all(torrents.map((torrent) => limiter.schedule(() => processTorrentRecord(torrent)))))
-    .then((resolved) => resolved.length > 0 && page < untilPage(category) ? scrapeLatestTorrentsForCategory(category, page + 1) : Promise.resolve());
+async function scrapeLatestTorrentsForCategory(category, page = 180) {
+  console.log(`Scrapping ${NAME} ${category} category page ${page}`);
+  return comoeubaixo
+      .browse({ category, page })
+      .catch((error) => {
+        console.warn(`Failed ${NAME} scrapping for [${page}] ${category} due: `, error);
+        return Promise.resolve([]);
+      })
+      .then((torrents) => Promise.all(torrents.map((torrent) => limiter.schedule(() => processEntry(torrent)))))
+      .then((resolved) => resolved.length > 0 && page < untilPage(category)
+          ? scrapeLatestTorrentsForCategory(category, page + 1)
+          : Promise.resolve());
 }
 
-async function processTorrentRecord(record) {
-  if (await checkAndUpdateTorrent({ provider: NAME, ...record })) {
-    return record;
+async function processEntry(entry) {
+  if (!entry.isTorrent) {
+    return entry;
   }
-  const torrentEntrys = await leetx
-    .torrent(record.torrentId)
-    .catch(() => undefined);
-  if (torrentEntrys === undefined) {
-    return Promise.resolve([])
-  }
-  return await Promise.allSettled(
-    torrentEntrys.map(async (torrentFound) => {
-      if (!torrentFound || !TYPE_MAPPING[torrentFound.category]) {
-        return Promise.resolve("Invalid torrent record");
-      }
-      if (isNaN(torrentFound.uploadDate)) {
-        console.warn(
-          `Incorrect upload date for [${torrentFound.infoHash}] ${torrentFound.name}`
-        );
-        return;
-      }
-      if (await checkAndUpdateTorrent(torrentFound)) {
-        return torrentFound;
-      }
-      if (!torrentFound.size) {
-        await updateTorrentSize(torrentFound)
-        .catch((err) => Promise.resolve(err))
-      }
-      if (!torrentFound.seeders) {
-        await updateCurrentSeeders(torrentFound)
-        .then(response => response.seeders === 0 ? delete response.seeders : response)
-      }
-      if (!torrentFound.imdbId) {
-        torrentFound.imdbId = await getImdbId(torrentFound.original_name, torrentFound.year, TYPE_MAPPING[torrentFound.category])
-      }
+  return comoeubaixo.torrent(entry.torrentId)
+      .then(records => Promises.sequence(records.map(record => () => processTorrentRecord(record))))
+      .catch(() => undefined);
+}
 
-      const torrent = {
-        infoHash: torrentFound.infoHash,
-        provider: NAME,
-        torrentId: torrentFound.torrentId,
-        name: torrentFound.original_name,
-        title: torrentFound.name.replace(/\t|\s+/g, " ").trim(),
-        type: TYPE_MAPPING[torrentFound.category],
-        year: torrentFound.year,
-        imdbId: torrentFound.imdbId,
-        uploadDate: torrentFound.uploadDate,
-        seeders: torrentFound.seeders,
-        size: torrentFound.size,
-        files: torrentFound.files
-      };
-      return createTorrentEntry(torrent);
-    })
-  );
+async function processTorrentRecord(foundTorrent) {
+  if (await checkAndUpdateTorrent({ provider: NAME, ...foundTorrent })) {
+    return foundTorrent;
+  }
+
+  if (!foundTorrent.size) {
+    await updateTorrentSize(foundTorrent);
+  }
+  if (!Number.isInteger(foundTorrent.seeders)) {
+    await updateCurrentSeeders(foundTorrent);
+  }
+
+  const torrent = {
+    infoHash: foundTorrent.infoHash,
+    provider: NAME,
+    torrentId: foundTorrent.torrentId,
+    title: foundTorrent.title,
+    type: TYPE_MAPPING[foundTorrent.category],
+    imdbId: foundTorrent.imdbId,
+    uploadDate: foundTorrent.uploadDate,
+    seeders: foundTorrent.seeders,
+    size: foundTorrent.size,
+    files: foundTorrent.files,
+    languages: foundTorrent.languages
+  };
+  return createTorrentEntry(torrent);
 }
 
 function typeMapping() {
   const mapping = {};
-  mapping[leetx.Categories.MOVIE] = Type.MOVIE;
-  mapping[leetx.Categories.TV] = Type.SERIES;
-  mapping[leetx.Categories.ANIME] = Type.ANIME;
-  mapping[leetx.Categories.DESENHOS] = Type.SERIES;
+  mapping[comoeubaixo.Categories.MOVIE] = Type.MOVIE;
+  mapping[comoeubaixo.Categories.TV] = Type.SERIES;
+  mapping[comoeubaixo.Categories.ANIME] = Type.ANIME;
+  mapping[comoeubaixo.Categories.DESENHOS] = Type.SERIES;
   return mapping;
 }
 
 function untilPage(category) {
-  if (leetx.Categories.DESENHOS === category) {
-    return 5;
+  if (comoeubaixo.Categories.DESENHOS === category) {
+    return UNTIL_PAGE;
   }
-  if (leetx.Categories.TV === category) {
-    return 5;
+  if (comoeubaixo.Categories.TV === category) {
+    return UNTIL_PAGE;
   }
   return UNTIL_PAGE;
 }
