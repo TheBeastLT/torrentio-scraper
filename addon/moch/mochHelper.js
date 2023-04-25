6889975485f79dc92101e15ee0268a6017c45f06
TheBeastLT <pauliox@beyond.lt>
2023-04-25 16:54:37 +0200

    remove meta id overriding when enriching

--- addon/moch/mochHelper.js
+++ addon/moch/mochHelper.js
@@ -25,7 +25,6 @@ async function enrichMeta(itemMeta) {
   if (commonImdbId) {
     return {
       ...itemMeta,
-      id: commonImdbId,
       logo: `${METAHUB_URL}/logo/medium/${commonImdbId}/img`,
       poster: `${METAHUB_URL}/poster/medium/${commonImdbId}/img`,
       background: `${METAHUB_URL}/background/medium/${commonImdbId}/img`,
