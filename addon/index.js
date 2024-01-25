a71a846dbd2bf264ca7e620279d4a5f658ecf834
TheBeastLT <pauliox@beyond.lt>
2024-01-26 00:09:18 +0200

    bring back sw stats

--- addon/index.js
+++ addon/index.js
@@ -1,9 +1,22 @@
 import express from 'express';
+import swStats from 'swagger-stats';
 import serverless from './serverless.js';
+import { manifest } from './lib/manifest.js';
 import { initBestTrackers } from './lib/magnetHelper.js';
 
 const app = express();
 app.enable('trust proxy');
+app.use(swStats.getMiddleware({
+  name: manifest().name,
+  version: manifest().version,
+  timelineBucketDuration: 60 * 60 * 1000,
+  apdexThreshold: 100,
+  authentication: true,
+  onAuthenticate: (req, username, password) => {
+    return username === process.env.METRICS_USER
+        && password === process.env.METRICS_PASSWORD
+  },
+}))
 app.use(express.static('static', { maxAge: '1y' }));
 app.use((req, res, next) => serverless(req, res, next));
 app.listen(process.env.PORT || 7000, () => {
