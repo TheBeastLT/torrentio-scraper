7714cb129af2b63d898059899ebd7c03fb3e737a
TheBeastLT <pauliox@beyond.lt>
2024-08-03 13:26:32 +0300

    remove default trackers

--- addon/lib/magnetHelper.js
+++ addon/lib/magnetHelper.js
@@ -7,28 +7,6 @@ import { extractProvider } from "./titleHelper.js";
 import { Providers } from "./filter.js";
 
 const TRACKERS_URL = 'https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt';
-const DEFAULT_TRACKERS = [
-  "udp://47.ip-51-68-199.eu:6969/announce",
-  "udp://9.rarbg.me:2940",
-  "udp://9.rarbg.to:2820",
-  "udp://exodus.desync.com:6969/announce",
-  "udp://explodie.org:6969/announce",
-  "udp://ipv4.tracker.harry.lu:80/announce",
-  "udp://open.stealth.si:80/announce",
-  "udp://opentor.org:2710/announce",
-  "udp://opentracker.i2p.rocks:6969/announce",
-  "udp://retracker.lanta-net.ru:2710/announce",
-  "udp://tracker.cyberia.is:6969/announce",
-  "udp://tracker.dler.org:6969/announce",
-  "udp://tracker.ds.is:6969/announce",
-  "udp://tracker.internetwarriors.net:1337",
-  "udp://tracker.openbittorrent.com:6969/announce",
-  "udp://tracker.opentrackr.org:1337/announce",
-  "udp://tracker.tiny-vps.com:6969/announce",
-  "udp://tracker.torrent.eu.org:451/announce",
-  "udp://valakas.rollo.dnsabr.com:2710/announce",
-  "udp://www.torrent.eu.org:451/announce",
-]
 const ANIME_TRACKERS = [
   "http://nyaa.tracker.wf:7777/announce",
   "http://anidex.moe:6969/announce",
@@ -71,8 +49,8 @@ export async function getMagnetLink(infoHash) {
 
 export async function initBestTrackers() {
   BEST_TRACKERS = await getBestTrackers();
-  ALL_ANIME_TRACKERS = unique(BEST_TRACKERS.concat(DEFAULT_TRACKERS).concat(ANIME_TRACKERS));
-  ALL_RUSSIAN_TRACKERS = unique(BEST_TRACKERS.concat(DEFAULT_TRACKERS).concat(RUSSIAN_TRACKERS));
+  ALL_ANIME_TRACKERS = unique(BEST_TRACKERS.concat(ANIME_TRACKERS));
+  ALL_RUSSIAN_TRACKERS = unique(BEST_TRACKERS.concat(RUSSIAN_TRACKERS));
   console.log('Retrieved best trackers: ', BEST_TRACKERS);
 }
 
