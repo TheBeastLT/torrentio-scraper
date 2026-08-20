9152a5215e87a30ee4a7ec7bb533712b0ddd81b2
TheBeastLT <pauliox@beyond.lt>
2026-08-20 17:42:43 +0300

    update static response videos

--- addon/moch/static.js
+++ addon/moch/static.js
@@ -1,14 +1,15 @@
 const staticVideoUrls = {
-  DOWNLOADING: `videos/downloading_v2.mp4`,
-  FAILED_DOWNLOAD: `videos/download_failed_v2.mp4`,
-  FAILED_ACCESS: `videos/failed_access_v2.mp4`,
-  FAILED_RAR: `videos/failed_rar_v2.mp4`,
-  FAILED_TOO_BIG: 'failed_too_big_v1.mp4',
-  FAILED_OPENING: `videos/failed_opening_v2.mp4`,
-  FAILED_UNEXPECTED: `videos/failed_unexpected_v2.mp4`,
-  FAILED_INFRINGEMENT: `videos/failed_infringement_v2.mp4`,
-  LIMITS_EXCEEDED: `videos/limits_exceeded_v1.mp4`,
-  BLOCKED_ACCESS: `videos/blocked_access_v1.mp4`,
+  DOWNLOADING: `videos/downloading_v3.mp4`,
+  FAILED_DOWNLOAD: `videos/download_failed_v3.mp4`,
+  FAILED_ACCESS: `videos/failed_access_v3.mp4`,
+  FAILED_RAR: `videos/failed_rar_v3.mp4`,
+  FAILED_TOO_BIG: `videos/failed_too_big_v2.mp4`,
+  FAILED_OPENING: `videos/failed_opening_v3.mp4`,
+  FAILED_UNEXPECTED: `videos/failed_unexpected_v3.mp4`,
+  FAILED_INFRINGEMENT: `videos/failed_infringement_v3.mp4`,
+  LIMITS_EXCEEDED: `videos/limits_exceeded_v2.mp4`,
+  BLOCKED_ACCESS: `videos/blocked_access_v2.mp4`,
+  FAILED_UNAVAILABLE: `videos/failed_unavailable_v1.mp4`,
 }
 
 
